f02ae086008f0e3d342b81d62cb53b2b084e41bb
GTNick <nickpp1111@yahoo.com>
2016-07-14 16:36:46 -0400

    Create Envoys.php

---
+++ scr/Envoys.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CrazierDevotee0\Envoys;
+
+use pocketmine\plugin\PluginBase;
+
+class Envoys extends PluginBase{
+
+    public function onEnable(){
+        $this->getLogger()->info("Envoys has been enabled!");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("Envoys has been disabled!");
+    }
+}
