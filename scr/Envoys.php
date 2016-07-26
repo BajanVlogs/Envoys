f3a9d5ae1088b7017ac54acb76cb94be58948618
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2016-07-25 23:41:53 -0400

    Will do the rest later

--- scr/Envoys.php
+++ scr/Envoys.php
@@ -6,10 +6,16 @@ use pocketmine\plugin\PluginBase;
 
 class Envoys extends PluginBase{
 
+public $location = [];
+public $ramdomX = mt_rand(-140, 208);
+public $ramdomY = mt_rand(-140, 208);
+
     public function onEnable(){
         $this->getLogger()->info("Envoys has been enabled!");
     }
-
+         public function randomChest(Level $level){
+              $chest = Block::get(Block::CHEST);
+         }
     public function onDisable(){
         $this->getLogger()->info("Envoys has been disabled!");
     }
