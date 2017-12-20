d09c6c8c55dd48f3c26bc3c53e32064c36596096
BajanVlogs <kjolly544@gmail.com>
2017-12-19 20:42:45 -0500

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -19,12 +19,12 @@ class Main extends PluginBase implements Listener{
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->items = (new Config($this->getDataFolder() ."Items.yml", Config::YAML))->getAll();
+		$this->items = (new Config($this->getDataFolder() ."resources\Items.yml", Config::YAML))->getAll();
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime*60*20);
 	}
 
 	public function runEnvoyEvent(){
-		$cfg = (new Config($this->getDataFolder() ."Envoys.yml", Config::YAML))->getAll();
+		$cfg = (new Config($this->getDataFolder() ."resources\Envoys.yml", Config::YAML))->getAll();
 		foreach($this->getServer()->getOnlinePlayers() as $player){
                         $player->sendMessage(TF::AQUA."WORLD EVENT");
 			$player->sendMessage(TF::GREEN."Envoys are being spawned in the warzone!");
@@ -43,7 +43,7 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function setEnvoy(Player $player){
-		$cfg = new Config($this->getDataFolder() ."Envoys.yml", Config::YAML);
+		$cfg = new Config($this->getDataFolder() ."resources\Envoys.yml", Config::YAML);
 		$cfg->set($player->x.":".$player->y.":".$player->z, $player->getLevel()->getName());
 		$cfg->save();
 		$player->getLevel()->setBlock($player->getPosition()->asVector3(), Block::get(54));
