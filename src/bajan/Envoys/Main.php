f35d70405400c9fbfea1f3b4f63e0101bb9e70e8
xXNiceYT <xXNiceAssasinl0@users.noreply.github.com>
2018-02-09 13:48:06 -0700

    Fixed Bug!

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -12,27 +12,36 @@ use pocketmine\block\Block;
 use pocketmine\item\Item;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\math\Vector3;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\tile\Tile;
 
 class Main extends PluginBase implements Listener{
+
 	//minutes
 	public $spawntime = 5;
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->items = (new Config($this->getDataFolder() ."resources\Items.yml", Config::YAML))->getAll();
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime*60*20);
+		@mkdir($this->getDataFolder());
 		$this->saveResource("Config.yml");
-                $this->saveResource("Enovys.yml");
-                $this->saveResource("Items.yml");
+		$this->saveResource("Envoys.yml");
+		$this->saveResource("Items.yml");
+		$this->cfg = new Config($this->getDataFolder()."Config.yml",Config::YAML);
+		$this->envoys = new Config($this->getDataFolder()."Envoys.yml",Config::YAML);
+		$this->items = new Config($this->getDataFolder()."Items.yml",Config::YAML);
 	}
 
 	public function runEnvoyEvent(){
-		$cfg = (new Config($this->getDataFolder() ."resources\Envoys.yml", Config::YAML))->getAll();
-		foreach($this->getServer()->getOnlinePlayers() as $player){
-                        $player->sendMessage(TF::AQUA."WORLD EVENT");
-			$player->sendMessage(TF::GREEN."Envoys are being spawned in the warzone!");
+		foreach($this->getServer()->getOnlinePlayers() as $players){
+			$players->sendMessage(TF::AQUA."WORLD EVENT");
+			$players->sendMessage(TF::GREEN."Envoys are being spawned in the warzone!");
 		}
-		foreach($cfg as $data => $level){
+		foreach($this->envoys as $data => $level){
 			$data = explode(":",$data);
 			$tile = $this->getServer()->getLevelByName($level)->getTile(new Vector3(intval($data[0]),intval($data[1]),intval($data[2])));
 			$i = rand(3,5);
@@ -45,21 +54,34 @@ class Main extends PluginBase implements Listener{
 		}
 	}
 
-	public function setEnvoy(Player $player){
-		$cfg = new Config($this->getDataFolder() ."resources\Envoys.yml", Config::YAML);
-		$cfg->set($player->x.":".$player->y.":".$player->z, $player->getLevel()->getName());
-		$cfg->save();
-		$player->getLevel()->setBlock($player->getPosition()->asVector3(), Block::get(54));
-		$player->sendMessage(TF::GREEN."Envoy set!");
+	public function setEnvoy(Player $sender){
+		$this->envoys->set($sender->x.":".$sender->y.":".$sender->z, $sender->getLevel()->getName());
+		$this->envoys->save();
+		$items = $this->items->get("Items");
+		$item = $items[array_rand($items)];
+		$values = explode(":", $item);
+		$level = $sender->getLevel();
+		$level->setBlock($sender->getPosition()->asVector3(), Block::get(54));
+		$nbt = new CompoundTag(" ", [
+			new ListTag("Items", []),
+			new StringTag("id", Tile::CHEST),
+			new IntTag("x", $sender->x),
+			new IntTag("y", $sender->y),
+			new IntTag("z", $sender->z)
+		]);
+		$chest = Tile::createTile("Chest", $sender->getLevel(), $nbt);
+		$level->addTile($chest);
+		$inv = $chest->getInventory();
+		$inv->addItem(Item::get($values[0], $values[1]));
+		$sender->sendMessage(TF::GREEN."Envoy set!");
 		return true;
 	}
 
-	
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		switch($cmd){
 			case "setenvoy":
-				if(!$player->hasPermission("envoy.set")) {$player->sendMessage(TF::RED."You do not have the required permission"); return false;}
-				$this->setEnvoy($player);
+				if(!$sender->hasPermission("envoy.set")) {$sender->sendMessage(TF::RED."You do not have the required permission"); return false;}
+				$this->setEnvoy($sender);
 				return true;
 		}
 	}
